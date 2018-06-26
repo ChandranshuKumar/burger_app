807dd3b7b8503e7da5e4ba366151f2b3f94a395d
Kumar Chandranshu <chandranshu040@gmail.com>
2018-06-26 04:16:02 +0000

    font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div>
+        <p>this is normal paragraph madafaka!!!!</p>
       </div>
     );
   }

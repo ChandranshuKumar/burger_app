cf31fdb7756e9ea7ede383b05fd15db927e040f0
Kumar Chandranshu <chandranshu040@gmail.com>
2018-06-29 12:23:35 +0000

    my orders component and orders link

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,36 +90,13 @@ class BurgerBuilder extends Component {
     
     purchaseContinueHandler = () => {
         // alert("You continue!!");
-        // this.setState({loading: true});
-        // const order =  {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Chandranshu',
-        //         address: {
-        //             street: 'Hirapur',
-        //             zipCode: 826001,
-        //             country: 'India'
-        //         },
-        //         email: 'abc@gmail.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // };
-        
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({loading: false, purchasing: false});
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading: false, purchasing: false});
-        //     });
         
         const queryparams = [];
         for ( let i in this.state.ingredients){
             queryparams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryparams.push('price=' + this.state.totalPrice);
         const queryString = queryparams.join('&');
-        
         this.props.history.push({
             pathname: '/checkout',
             search: '?' + queryString

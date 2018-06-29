3e4077807246536b6edaee7c8a2dfc4b60f4f64d
Kumar Chandranshu <chandranshu040@gmail.com>
2018-06-29 12:25:36 +0000

    toFixed()

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -31,7 +31,7 @@ const order = (props) => {
     return(
         <div className={classes.Order}>
             <p>Ingredients: {ingredientOutput}</p>
-            <p>Price: <strong>Rs {props.price}</strong></p>
+            <p>Price: <strong>Rs {props.price.toFixed(2)}</strong></p>
         </div>
     );
 };

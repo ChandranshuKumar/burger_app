3098d0b627ff3d3cf0d7168504161d3538fd509e
Kumar Chandranshu <chandranshu040@gmail.com>
2018-06-29 10:08:05 +0000

    added contact form

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,29 +90,41 @@ class BurgerBuilder extends Component {
     
     purchaseContinueHandler = () => {
         // alert("You continue!!");
-        this.setState({loading: true});
-        const order =  {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Chandranshu',
-                address: {
-                    street: 'Hirapur',
-                    zipCode: 826001,
-                    country: 'India'
-                },
-                email: 'abc@gmail.com'
-            },
-            deliveryMethod: 'fastest'
-        };
+        // this.setState({loading: true});
+        // const order =  {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Chandranshu',
+        //         address: {
+        //             street: 'Hirapur',
+        //             zipCode: 826001,
+        //             country: 'India'
+        //         },
+        //         email: 'abc@gmail.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // };
         
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-            });
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false, purchasing: false});
+        //     });
+        
+        const queryparams = [];
+        for ( let i in this.state.ingredients){
+            queryparams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        const queryString = queryparams.join('&');
+        
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
+        
     }
     
     render(){
